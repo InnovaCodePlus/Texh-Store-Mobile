3dff10c5771523cfd6df476dbd8bd18c52d7b771
Mattias <duartemattias3@gmail.com>
2023-03-30 15:27:15 -0400

    clase-01

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Hola Mundo</Text>
+            <Text>Hola Mundo 2</Text>
+        </View>
+    )
+}
+
+export default Home;
